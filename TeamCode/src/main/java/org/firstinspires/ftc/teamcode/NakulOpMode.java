bb53ae38fe1ea5c8d43e78389943c669f26c632c
Nakul K <thedynamoknight@gmail.com>
2023-07-02 19:55:00 -0700

    Added Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NakulOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NakulOpMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+// The hardware object is referenced
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
+//@Autonomous
 
 public class NakulOpMode extends LinearOpMode {
 
@@ -18,7 +20,7 @@ public class NakulOpMode extends LinearOpMode {
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
 
-    // This method must be implemented, the override allows one to change the method
+    // This method must be implemented. It is overridden to do the stuff we want.
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(Gyroscope.class, "imu");
@@ -33,10 +35,18 @@ public class NakulOpMode extends LinearOpMode {
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
+        int count = 0;
+        double tgtPower = 0;
         while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
-            telemetry.update();
+            // Allows left stick movement
+            tgtPower = -this.gamepad1.left_stick_y;
+            motorTest.setPower(tgtPower);
+            telemetry.addData("Target Power", tgtPower);
+            telemetry.addData("Motor Power", motorTest.getPower()); // The same as previous
 
+            telemetry.addData("Status", "Running: Nakul is" + count + "% cool!");
+            telemetry.update();
+            count++;
         }
     }
 
