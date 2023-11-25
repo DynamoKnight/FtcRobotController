2aa4ce05ac4ff7df03ea6e36f7d7e9ac2294dc47
Nakul K <thedynamoknight@gmail.com>
2023-11-25 10:40:54 -0800

    Camera detects blue and red, Auto does not work!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueBackAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueBackAuto.java
@@ -19,7 +19,6 @@ public class BlueBackAuto extends LinearOpMode {
     //VARIABLES
     //////////////////////
     public NakulAuto auto;
-
     //////////////////////
     //METHODS
     //////////////////////
